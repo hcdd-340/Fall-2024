107394e3ad367bee458adf4d975781964ca189f9
hcdd-340 <me@saeedabdullah.com>
2022-09-11 20:03:02 -0400

    Completes activity 4

--- activities/PennStateId/app/src/main/java/edu/psu/ist/hcdd340/pennstateid/MainActivity.java
+++ activities/PennStateId/app/src/main/java/edu/psu/ist/hcdd340/pennstateid/MainActivity.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.material.imageview.ShapeableImageView;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private static final String TAG = "ACTIVITY_4";
@@ -31,5 +33,53 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // what's the id number that is currently being displayed?
         String currentProfileId = idNumberTextView.getText().toString();
         Log.d(TAG, String.format("Current profile id: %s", currentProfileId));
+
+        // let's compare with the ID assigned to the president
+        // we will have to convert the ID (an int) to the assigned String value
+        String presidentId = this.getString(R.string.president_id_number);
+
+        if (presidentId.equals(currentProfileId)) {
+            // currently the president's profile is being shown
+            // we will replace it with the dean's profile
+            showProfile(R.drawable.ist_dean,
+                    this.getString(R.string.ist_dean_first_name),
+                    this.getString(R.string.ist_dean_last_name),
+                    this.getString(R.string.ist_dean_machine_id),
+                    this.getString(R.string.ist_dean_id_number),
+                    this.getString(R.string.ist_dean_position_description));
+        } else {
+            // currently the dean's profile is being shown
+            // we will replace it with the president's profile
+            showProfile(R.drawable.psu_president,
+                    this.getString(R.string.president_first_name),
+                    this.getString(R.string.president_last_name),
+                    this.getString(R.string.president_machine_id),
+                    this.getString(R.string.president_id_number),
+                    this.getString(R.string.president_position_description));
+        }
+    }
+
+    private void showProfile(int profileImageId, String firstName, String lastName,
+                             String machineId, String idNumber, String positionDescription) {
+        // update profile image
+        ShapeableImageView idHolderImage = findViewById(R.id.id_holder_image);
+        idHolderImage.setImageResource(profileImageId);
+
+        // first name
+        TextView label = findViewById(R.id.first_name);
+        label.setText(firstName);
+
+        label = findViewById(R.id.last_name);
+        label.setText(lastName);
+
+        label = findViewById(R.id.machine_id);
+        label.setText(machineId);
+
+        label = findViewById(R.id.id_number);
+        label.setText(idNumber);
+
+        label = findViewById(R.id.position_description);
+        label.setText(positionDescription);
+
     }
 }
