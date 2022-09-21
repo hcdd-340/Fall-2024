f1b96df23c5ff882060697f48d438a360e9de46c
hcdd-340 <me@saeedabdullah.com>
2022-09-20 21:48:14 -0400

    Adds edit text for email

--- activities/PennStateId/app/src/main/java/edu/psu/ist/hcdd340/pennstateid/LoginActivity.java
+++ activities/PennStateId/app/src/main/java/edu/psu/ist/hcdd340/pennstateid/LoginActivity.java
@@ -3,12 +3,28 @@ package edu.psu.ist.hcdd340.pennstateid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private final static String TAG = "LOG_IN";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button button = findViewById(R.id.button_login);
+        button.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        EditText emailEditText = findViewById(R.id.editTextEmail);
+        String email = emailEditText.getText().toString();
+        Log.d(TAG, "Email entered: " + email);
     }
 }
