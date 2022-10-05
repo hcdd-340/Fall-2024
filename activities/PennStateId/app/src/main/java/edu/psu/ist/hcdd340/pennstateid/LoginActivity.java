b86a31f6287413cbaab1eb87a818840017f68dad
hcdd-340 <me@saeedabdullah.com>
2022-10-04 21:21:09 -0400

    Adds snackbars and an alert dialog

--- activities/PennStateId/app/src/main/java/edu/psu/ist/hcdd340/pennstateid/LoginActivity.java
+++ activities/PennStateId/app/src/main/java/edu/psu/ist/hcdd340/pennstateid/LoginActivity.java
@@ -1,7 +1,9 @@
 package edu.psu.ist.hcdd340.pennstateid;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -9,6 +11,8 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -31,6 +35,23 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         EditText passwordEditText = findViewById(R.id.editTextPassword);
         String password = passwordEditText.getText().toString();
         Log.d(TAG, "Email: " + email + " and Password: " + password);
+
+        /** toy example — DO NOT do this in production code! **/
+        String userAllowed = "hcdd340@psu.edu";
+        String correctPassword = "IST";
+        if (email.equals(userAllowed) &&
+                password.equals(correctPassword)) {
+            Button button = findViewById(R.id.button_login);
+            Snackbar.make(button,
+                    "Logged In!",
+                    Snackbar.LENGTH_LONG).show();
+        } else {
+            AlertDialog.Builder d = new AlertDialog.Builder(this);
+            d.setTitle(R.string.login_error_title);
+            d.setMessage(R.string.login_error_message);
+            d.setPositiveButton(android.R.string.ok, null);
+            d.show();
+        }
     }
 
     @Override
@@ -42,12 +63,17 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int menuId = item.getItemId();
-
         if (menuId == R.id.menu_info) {
-            Log.d(TAG, "Info menu clicked!");
+            Button button = findViewById(R.id.button_login);
+            Snackbar.make(button,
+                    R.string.app_info,
+                    Snackbar.LENGTH_LONG).show();
             return true;
         } else if (menuId == R.id.menu_register) {
-            Log.d(TAG, "Register menu clicked!");
+            Button button = findViewById(R.id.button_login);
+            Snackbar.make(button,
+                    "Registration not implemented yet!",
+                    Snackbar.LENGTH_SHORT).show();
             return true;
         }
         return super.onOptionsItemSelected(item);
